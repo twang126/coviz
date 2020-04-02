7246d23878d1e393ac84cfb895a786e62da57c71
Tim Wang <timtim305@gmail.com>
2020-04-02 13:43:44 -0400

    COVID to COVID-19

--- utils/streamlit_ui.py
+++ utils/streamlit_ui.py
@@ -5,7 +5,7 @@ import altair as alt
 
 
 def add_header_and_title(st):
-    st.title("The COVID Analysis Project")
+    st.title("The COVID-19 Analysis Project")
     st.markdown(
         """Authors: [Tim Wang](https://www.timothyxwang.com) & [Anurag Pradhan](https://www.linkedin.com/in/anuragpradhan2016) | [Github](https://github.com/twang126/coviz)"""
     )
@@ -16,7 +16,7 @@ def add_header_and_title(st):
         many existing visualizations can only operate on subsets of this data, not the whole. We have started this project to attempt to consolidate
         all of these different datasets under one common schema and facilitate analytics on all of the world's available, reliable data on the virus.
 
-        The **COVID Analysis Project** has two main goals:  
+        The **COVID-19 Analysis Project** has two main goals:  
         &ensp;&ensp;1. Create a central repository and schema for all of the reliable data reported in the world  
         &ensp;&ensp;2. Allow for queries and visualizations against this data  
 
